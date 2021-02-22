d82335d7b72b829770133c2e7568eca217647329
Exjp <Exjp@jacpourtier@tutanota.com>
2021-02-22 16:01:34 +0100

    add xmlManager + func generation of the xml

---
+++ xmlManager.py
@@ -0,0 +1,25 @@
+from xml import etree
+import xml.etree.cElementTree as ET
+from io import BytesIO
+
+import os
+
+def emptyXml():
+   
+    root = ET.Element("users")
+    doc = ET.SubElement(root, "user")
+    
+    ET.SubElement(doc, "number").text = "some value1"
+    ET.SubElement(doc, "key").text = "some vlaue2"
+    tree = ET.ElementTree(root)
+    tree.write("page.xml",
+           xml_declaration=True,encoding='utf-8',
+           method="xml")
+
+    # tree.write("filename.xml")
+
+def main():
+    emptyXml()
+
+if __name__ == "__main__":
+    main()
