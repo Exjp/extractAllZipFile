451f99740a51a25e4239b1aa24f53ba6aa6ef5e6
glartigue <ghislain.lartigue@etu.u-bordeaux.fr>
2021-02-22 16:44:12 +0100

    ajout d'utilisateur dans la bdd

--- xmlManager.py
+++ xmlManager.py
@@ -1,25 +1,56 @@
-from xml import etree
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as ET
 from io import BytesIO
 
 import os
 
+
+def init():
+    emptyXml()
+    global tree
+    tree = ET.parse('page.xml')
+    global root
+    root = tree.getroot()
+
+
 def emptyXml():
    
-    root = ET.Element("users")
-    doc = ET.SubElement(root, "user")
-    
-    ET.SubElement(doc, "number").text = "some value1"
-    ET.SubElement(doc, "key").text = "some vlaue2"
-    tree = ET.ElementTree(root)
-    tree.write("page.xml",
+    rootEmpty = ET.Element("users")
+    treeEmpty = ET.ElementTree(rootEmpty)
+    treeEmpty.write("page.xml",
            xml_declaration=True,encoding='utf-8',
            method="xml")
 
-    # tree.write("filename.xml")
+
+# vérifier que les champs sont uniques, vérifier que les champs sont corrects
+def addUser(aliasValue, numberValue, keyValue):
+    user = ET.Element('user')
+    user.set("alias", aliasValue)
+    number = ET.SubElement(user, "number")
+    number.text = numberValue
+    key = ET.SubElement(user, "key")
+    key.text = keyValue
+    root.append(user)
+
+
+# Element.get pour trouver les attributs
+def removeUser():
+    print("TODO")
+
+
+def getNumberFromAlias():
+    print("TODO")
+
+
+def getAliases():
+    print("TODO")
+
 
 def main():
-    emptyXml()
+    addUser("Thierry", "+33666666666", "key")
+    addUser("Jak", "+33626486623", "key2")
+    tree.write('page.xml', encoding="utf-8", xml_declaration=True)
 
 if __name__ == "__main__":
+    init()
     main()
+
